2fedd68d8d3e1d125b6a3f7d796781f9f0360ff4
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-03-08 12:16:16 +0200

    hw

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,5 +2,16 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+enum Weekend {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
 
-function isWeekend(params: type) {}
+function isWeekend(day: Weekend): boolean {
+  return day === Weekend.Saturday || day === Weekend.Sunday;
+}
--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -2,7 +2,10 @@
   Створіть тип "Gender", використовуючи union type, 
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
+type Gender = "male" | "female";
+let myGender: Gender;
 
-const myGender;
+myGender = "male";
+myGender = "female";
 
 export {};
